07529fef90783a4604d8c0737976b84d6c56ab7d
Tzeentche <ilyasugako87@gmail.com>
2023-06-24 11:45:55 +0300

    Add Answer-classes, refactored SaveStudentOrder, add and refactored StudentOrderValidator, relaited it with Answer-classes

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,10 +1,20 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
 
-        saveStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Trofim";
+        so.hLastName = "Trifonov";
+        so.wFirstName = "Parafia";
+        so.wLastName = "Trifonova";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
     }
 
-    static void saveStudentOrder() {
-        System.out.println("SaveStudentOrder");
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 1999;
+        System.out.println("SaveStudentOrder" + studentOrder.hLastName);
+
+        return answer;
     }
 }
