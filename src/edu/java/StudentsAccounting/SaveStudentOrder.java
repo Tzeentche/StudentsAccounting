9a44af92c4c1625bbaa874c235f478d5331d0a9e
Tzeentche <ilyasugako87@gmail.com>
2023-06-27 15:36:31 +0300

    Add Child,Adult,Person,geters/seters,rectored SSO&SO

--- src/edu/java/StudentsAccounting/SaveStudentOrder.java
+++ src/edu/java/StudentsAccounting/SaveStudentOrder.java
@@ -1,21 +1,11 @@
 package edu.java.StudentsAccounting;
 
+import edu.java.StudentsAccounting.domain.Adult;
 import edu.java.StudentsAccounting.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
-
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Trofim";
-        so.hLastName = "Trifonov";
-        so.wFirstName = "Parafia";
-        so.wLastName = "Trifonova";
-
-        StudentOrder so1 = new StudentOrder();
-        so1.hFirstName = "Trofim";
-        so1.hLastName = "Trifonov";
-        so1.wFirstName = "Parafia";
-        so1.wLastName = "Trifonova";
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -23,8 +13,17 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 1999;
-        System.out.println("edu.java.StudentsAccounting.SaveStudentOrder" + studentOrder.hLastName);
+        System.out.println("SaveStudentOrder");
 
         return answer;
+    }
+
+    static StudentOrder buildStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        Adult husband = new Adult();
+        husband.setGivenName("Trifon");
+        so.setHusband(husband);
+
+        return so;
     }
 }
---
+++ src/edu/java/StudentsAccounting/domain/Person.java
@@ -0,0 +1,43 @@
+package edu.java.StudentsAccounting.domain;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String surname;
+    private String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
