e71897da32cc0c3f94c3533bbe28135009ea0566
Tzeentche <ilyasugako87@gmail.com>
2023-07-10 23:55:44 +0300

    Add exception class

--- src/edu/java/StudentsAccounting/validator/RealCityRegisterChecker.java
+++ src/edu/java/StudentsAccounting/validator/RealCityRegisterChecker.java
@@ -2,10 +2,11 @@ package edu.java.StudentsAccounting.validator;
 
 import edu.java.StudentsAccounting.domain.CityRegisterCheckerResponse;
 import edu.java.StudentsAccounting.domain.Person;
+import edu.java.StudentsAccounting.exception.CityRegisterException;
 
 public class RealCityRegisterChecker implements CityRegisterChecker {
 
-    public CityRegisterCheckerResponse checkPerson(Person person) {
+    public CityRegisterCheckerResponse checkPerson(Person person) throws CityRegisterException {
 
 
         return null;
