8a3b98a4229e49e4acb8c3a768393569ea4ff040
Tzeentche <ilyasugako87@gmail.com>
2023-07-17 23:30:10 +0300

    Add exception for GRN

--- src/edu/java/StudentsAccounting/validator/FakeCityRegisterChecker.java
+++ src/edu/java/StudentsAccounting/validator/FakeCityRegisterChecker.java
@@ -5,6 +5,7 @@ import edu.java.StudentsAccounting.domain.Child;
 import edu.java.StudentsAccounting.domain.register.CityRegisterResponse;
 import edu.java.StudentsAccounting.domain.Person;
 import edu.java.StudentsAccounting.exception.CityRegisterException;
+import edu.java.StudentsAccounting.exception.TransportException;
 
 public class FakeCityRegisterChecker implements CityRegisterChecker {
 
@@ -14,8 +15,10 @@ public class FakeCityRegisterChecker implements CityRegisterChecker {
     private static final String BAD_2 = "2001";
     private static final String ERR_1 = "1002";
     private static final String ERR_2 = "2002";
+    private static final String ERR_T_1 = "1003";
+    private static final String ERR_T_2 = "2003";
 
-    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException {
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
 
         CityRegisterResponse res = new CityRegisterResponse();
 
@@ -33,7 +36,12 @@ public class FakeCityRegisterChecker implements CityRegisterChecker {
             }
 
             if(ps.equals(ERR_1) || ps.equals(ERR_2)) {
-                CityRegisterException ex = new CityRegisterException("Fake ERROR " + ps);
+                CityRegisterException ex = new CityRegisterException("1", "GRN ERROR " + ps);
+                throw ex;
+            }
+
+            if(ps.equals(ERR_T_1) || ps.equals(ERR_T_2)) {
+                TransportException ex = new TransportException("Transport ERROR " + ps);
                 throw ex;
             }
         }
--- src/edu/java/StudentsAccounting/validator/RealCityRegisterChecker.java
+++ src/edu/java/StudentsAccounting/validator/RealCityRegisterChecker.java
@@ -3,11 +3,11 @@ package edu.java.StudentsAccounting.validator;
 import edu.java.StudentsAccounting.domain.register.CityRegisterResponse;
 import edu.java.StudentsAccounting.domain.Person;
 import edu.java.StudentsAccounting.exception.CityRegisterException;
+import edu.java.StudentsAccounting.exception.TransportException;
 
 public class RealCityRegisterChecker implements CityRegisterChecker {
 
-    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException {
-
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
 
         return null;
     }
